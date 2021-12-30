7fc78facdb403c8394c2a7498f606c8abbe4955f
kitsune <aow.anti.hero@gmail.com>
2021-12-30 15:36:21 +0800

    completing help command

--- commands/general/help.js
+++ commands/general/help.js
@@ -9,22 +9,111 @@ export const requireArgs = false;
 export const commandType = COMMAND_TYPE.GENERAL;
 export const cooldown = 1;
 
+const OWNER               = `commands.${canonName}.owner-commands`;
+const ADMIN               = `commands.${canonName}.admin-commands`;
+const GENERAL             = `commands.${canonName}.general-commands`;
+const OWNER_FLAGS         = `commands.${canonName}.flags.owner`;
+const ADMIN_FLAGS         = `commands.${canonName}.flags.admin`;
+const GENERAL_FLAGS       = `commands.${canonName}.flags.general`;
+
+/**
+ * List owner commands
+ * @param {CommandContext} context
+ * @return {boolean}
+ */
+function listOwner(context) {
+  const {client, lang, channel, message} = context;
+  const l10n = client.l10n;
+
+  if (!context.hasOwnerPermission) return false;
+
+  const commandList = client.commands
+      .filter((cmd)=>cmd.commandType==COMMAND_TYPE.OWNER)
+      .map((cmd)=>cmd.name)
+      .join(l10n.s(lang, 'delimiter'));
+
+  const response = l10n.t( lang, OWNER, '{COMMANDS}', commandList);
+
+  channel.send({
+    content: response, reply: {messageReference: message.id},
+  });
+  return true;
+}
+
+/**
+ * List guild administrator commands
+ * @param {CommandContext} context
+ * @return {boolean}
+ */
+function listAdmin(context) {
+  const {client, lang, channel, message} = context;
+  const l10n = client.l10n;
+
+  if (!context.hasAdminPermission) return false;
+
+  const commandList = client.commands
+      .filter((cmd)=>cmd.commandType==COMMAND_TYPE.ADMIN)
+      .map((cmd)=>cmd.name)
+      .join(l10n.s(lang, 'delimiter'));
+  const response = l10n.t(lang, ADMIN, '{COMMANDS}', commandList);
+
+  channel.send({
+    content: response, reply: {messageReference: message.id},
+  });
+  return true;
+}
+
+/**
+ * List general commands
+ * @param {CommandContext} context
+ * @return {boolean}
+ */
+function listGeneral(context) {
+  const {client, lang, channel, message} = context;
+  const l10n = client.l10n;
+
+  const commandList = client.commands
+      .filter((cmd)=>cmd.commandType==COMMAND_TYPE.GENERAL)
+      .map((cmd)=>cmd.name)
+      .join(l10n.s(lang, 'delimiter'));
+  const response = l10n.t(lang, GENERAL, '{COMMANDS}', commandList);
+
+  channel.send({
+    content: response, reply: {messageReference: message.id},
+  });
+  return true;
+}
+
 /**
  * @param {CommandContext} context
  * @return {Promise<Discord.Message>}
  */
 export async function execute(context) {
   const {client, channel, message, lang, commandPrefix, args} = context;
+  const l10n = client.l10n;
 
-  if (args.length == 0) return; // TODO: list all commands
+  if (args.length == 0) return listGeneral(context);
 
-  const __cmdAlias = args[0];
-  const __cmdName =
-    client.l10n.getCanonicalName(lang, 'aliases.commands', __cmdAlias);
+  const firstArg = args[0].toLowerCase();
+  const ownerFlags = l10n.s(lang, OWNER_FLAGS);
+  const adminFlags = l10n.s(lang, ADMIN_FLAGS);
+  const generalFlags = l10n.s(lang, GENERAL_FLAGS);
 
-  if (!__cmdName) return false;
+  if (ownerFlags.includes(firstArg)) return listOwner(context);
+  if (adminFlags.includes(firstArg)) return listAdmin(context);
+  if (generalFlags.includes(firstArg)) return listGeneral(context);
 
-  const __cmd = client.commands.get(__cmdName);
+  const __cmdAlias =
+    firstArg.startsWith(commandPrefix)?
+    firstArg.slice(commandPrefix.length):
+    firstArg;
+
+  const __cmdCanonName =
+    l10n.getCanonicalName(lang, 'aliases.commands', __cmdAlias);
+
+  if (!__cmdCanonName) return false;
+
+  const __cmd = client.commands.get(__cmdCanonName);
 
   switch (__cmd.commandType) {
     case COMMAND_TYPE.OWNER:
@@ -35,7 +124,7 @@ export async function execute(context) {
       break;
   }
 
-  const helpTxt = client.l10n.getCommandHelp(lang, __cmdName);
+  const helpTxt = l10n.getCommandHelp(lang, __cmdCanonName);
   if (helpTxt) {
     channel.send({
       content: '```' + helpTxt.replaceAll('?', commandPrefix) + '```',
