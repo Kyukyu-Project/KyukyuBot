0e5396a44b5873e6ed191e9aa91d8d6cc628fc26
kitsune <aow.anti.hero@gmail.com>
2022-05-16 15:37:32 +0800

    navigable info box

--- commands/aow/info.js
+++ commands/aow/info.js
@@ -12,6 +12,8 @@ export const commandPerm = COMMAND_PERM.GENERAL;
 export const cooldown = 5;
 
 const optSubjectLabel  = 'subject';
+const optTagLabel      = 'tag';
+const optMessageLabel  = 'message';
 
 /**
  * @param {CommandContext} context
@@ -23,6 +25,8 @@ export function getSlashData(context) {
 
   const cHint = l10n.s(lang, `commands.${canonName}.c-hint`);
   const optSubjectHint = l10n.s(lang, `commands.${canonName}.opt-subject-hint`);
+  const optTagHint = l10n.s(lang, `commands.${canonName}.opt-tag-hint`);
+  const optMessageHint = l10n.s(lang, `commands.${canonName}.opt-message-hint`);
 
   const directory = new Map();
 
@@ -57,6 +61,16 @@ export function getSlashData(context) {
                     .sort((a, b) => a[0].localeCompare(b[0], lang))
                     .map((subject) => [subject, subject]),
             ),
+        )
+        .addUserOption((option) => option
+            .setName(optTagLabel)
+            .setDescription(optTagHint)
+            .setRequired(false),
+        )
+        .addStringOption((option) => option
+            .setName(optMessageLabel)
+            .setDescription(optMessageHint)
+            .setRequired(false),
         ));
   }
 
@@ -71,25 +85,116 @@ export async function slashExecute(context) {
   const {client, lang, interaction} = context;
   const {l10n} = client;
 
-  const subject = interaction.options.getString('subject');
+  const subject = interaction.options.getString(optSubjectLabel);
   const info = l10n.s(lang, 'aow.info')?.find((el) => el.name === subject);
 
   if (info) {
-    const embeds = info.embed?[info.embed]:info.embeds;
+    const userId = interaction.user.id;
+    const taggedId = interaction.options.getUser(optTagLabel)?.id || null;
+    const message = interaction.options.getString(optMessageLabel) || null;
 
-    // cannot send Embed using interaction.reply due to Discord API bug #2612
-    interaction.channel.send({
-      content: l10n.t(
-          lang, 'messages.use-command',
-          '{PREFIX}', '/',
-          '{COMMAND}', name,
-          '{USER ID}', interaction.user.id),
-      embeds: embeds,
-    });
-    interaction.reply({
-      content: l10n.s(lang, 'messages.info-sent'),
-      ephemeral: true,
-    });
+    let content = l10n.t(
+        lang, 'messages.use-command',
+        '{PREFIX}', '/',
+        '{COMMAND}', name,
+        '{USER ID}', userId);
+
+    if (taggedId) {
+      if (message) {
+        content += '\n' + l10n.t(
+            lang, 'messages.tag-user-with-message',
+            '{USER ID}', userId,
+            '{TAGGED ID}', taggedId,
+            '{MESSAGE}', message);
+      } else {
+        content += '\n' + l10n.t(
+            lang, 'messages.tag-user',
+            '{USER ID}', userId,
+            '{TAGGED ID}', taggedId);
+      }
+    }
+
+    if (info.embeds) { // tabbed
+      const tabOptions = info.embeds.map((embed, index) =>
+        embed.description?
+        ({
+          label: embed.title,
+          value: index.toString(),
+          description: embed.description,
+        }):
+        ({
+          label: embed.title,
+          value: index.toString(),
+        }),
+      );
+
+      const components = [{
+        type: 1,
+        components: [{
+          type: 3,
+          custom_id: `${name}.select`,
+          placeholder: l10n.s(lang, `${name}.select-placeholder`),
+          options: tabOptions,
+        }],
+      }];
+
+      let currentEmbed = info.embeds[0];
+
+      // cannot send Embed using interaction.reply due to Discord API bug #2612
+      interaction.channel.send({
+        content: content,
+        embeds: [currentEmbed],
+        components: components,
+      }).then((response) => {
+        const collector = response.createMessageComponentCollector({
+          componentType: 'SELECT_MENU',
+          time: 10 * 60 * 1000,
+          idle: 1 * 60 * 1000,
+        });
+
+        collector.on('collect',
+            async (i) => {
+              if (i.user.id === userId || i.user.id === taggedId) {
+                currentEmbed = info.embeds[parseInt(i.values[0])];
+                i.message.edit({
+                  content: content,
+                  embeds: [currentEmbed],
+                  components: components,
+                });
+                i.deferUpdate();
+              } else {
+                i.reply({
+                  content: l10n.s(lang, 'messages.no-interaction-permission'),
+                  ephemeral: true,
+                });
+              }
+            },
+        );
+
+        collector.on('end', (collected) => {
+          response.edit({
+            content: content, embeds: [currentEmbed], components: [],
+          });
+        });
+
+        interaction.reply({
+          content: l10n.s(lang, 'messages.info-sent'),
+          ephemeral: true,
+        });
+      });
+    } else { // non-tabbed
+      // cannot send Embed using interaction.reply due to Discord API bug #2612
+      interaction.channel.send({
+        content: content,
+        embeds: [info.embed],
+      }).then((response) => {
+        interaction.reply({
+          content: l10n.s(lang, 'messages.info-sent'),
+          ephemeral: true,
+        });
+      });
+    }
+
     return true;
   } else {
     interaction.reply({content: l10n.s(lang, NO_INFO), ephemeral: true});
