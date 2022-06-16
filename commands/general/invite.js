e65533270014f5d4ef883e97156f4db4e3d077d7
kitsune <aow.anti.hero@gmail.com>
2022-06-16 12:01:21 +0800

    add /invite command
    make adding bot easier

---
+++ commands/general/invite.js
@@ -0,0 +1,49 @@
+/**
+ * @typedef {import('../../src/typedef.js').CommandContext} CommandContext
+ * @typedef {import('../../src/typedef.js').InteractionContext} IContext
+ */
+import {COMMAND_PERM} from '../../src/typedef.js';
+import {SlashCommandBuilder} from '@discordjs/builders';
+
+export const canonName = 'general.invite';
+export const name = 'invite';
+export const requireArgs = false;
+export const commandPerm = COMMAND_PERM.GENERAL;
+export const cooldown = 0;
+
+/**
+ * @param {CommandContext|IContext} context
+ * @return {ContextMenuCommandBuilder}
+ */
+export function getSlashData(context) {
+  const {client, lang} = context;
+  const {l10n} = client;
+
+  const cHint = l10n.s(lang, `commands.${canonName}.c-hint`);
+
+  return new SlashCommandBuilder()
+      .setName(name)
+      .setDescription(cHint);
+}
+
+/**
+ * @param {IContext} context
+ * @return {boolean} - true if command is executed
+ */
+export async function slashExecute(context) {
+  const {client, lang, interaction} = context;
+  const {l10n} = client;
+
+  const inviteLink =
+      'https://discord.com/api/oauth2/authorize?client_id=' +
+      client.application.id +
+      '&permissions=277294204992&scope=bot%20applications.commands';
+
+  const response = l10n.t(
+      lang,
+      `commands.${canonName}.response`,
+      '{INVITE LINK}', inviteLink);
+
+  interaction.reply({content: response, ephemeral: true});
+  return true;
+}
