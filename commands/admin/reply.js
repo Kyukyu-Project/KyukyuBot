af6f1530b6dd66a7c97eeaea0029f70ef8487fa7
kitsune <aow.anti.hero@gmail.com>
2022-07-08 16:08:51 +0800

    improved reply-thru-bot

--- commands/admin/reply.js
+++ commands/admin/reply.js
@@ -1,11 +1,12 @@
 /**
  * @typedef {import('../../src/typedef.js').DeploymentContext} DeploymentContext
- * @typedef {import('../../src/typedef.js').CommandContext} CommandContext
  * @typedef {import('../../src/typedef.js').InteractionContext} IContext
  */
 
 import {COMMAND_PERM} from '../../src/typedef.js';
-import {SlashCommandBuilder} from '@discordjs/builders';
+import {ContextMenuCommandBuilder} from '@discordjs/builders';
+
+import {Modal, MessageActionRow, TextInputComponent} from 'discord.js';
 
 import {l10n} from '../../src/l10n.js';
 
@@ -15,88 +16,60 @@ export const requireArgs = true;
 export const commandPerm = COMMAND_PERM.MODERATOR;
 export const cooldown = 0;
 
-const optResponseLabel = 'response';
-const optMessageIdLabel = 'message-id';
-const optChannelLabel = 'channel';
+let eventHandlerAttached = false;
 
 /**
  * @param {DeploymentContext} context
- * @return {SlashCommandBuilder}
+ * @return {ContextMenuCommandBuilder}
  */
 export function getSlashData(context) {
-  const {lang} = context;
-
-  const cHint =
-      l10n.s(lang, `commands.${canonName}.c-hint`);
-  const optResponseHint  =
-      l10n.s(lang, `commands.${canonName}.opt-response-hint`);
-  const optMessageIdHint =
-      l10n.s(lang, `commands.${canonName}.opt-message-id-hint`);
-  const optChannelHint =
-      l10n.s(lang, `commands.${canonName}.opt-channel-hint`);
-
-  return new SlashCommandBuilder()
-      .setName(name)
-      .setDescription(cHint)
-      .addStringOption((option) => option
-          .setName(optResponseLabel)
-          .setDescription(optResponseHint)
-          .setRequired(true),
-      )
-      .addStringOption((option) => option
-          .setName(optMessageIdLabel)
-          .setDescription(optMessageIdHint)
-          .setRequired(true),
-      )
-      .addChannelOption((option) => option
-          .setName(optChannelLabel)
-          .setDescription(optChannelHint)
-          .setRequired(false),
-      );
+  return new ContextMenuCommandBuilder().setName(name).setType(3);
 }
+
+const modalEventHandler = async (i) => {
+  if (!i.isModalSubmit()) return;
+  i.deferUpdate();
+  const {customId, fields, channel} = i;
+  if (customId.startsWith('reply.')) {
+    const respondTo = customId.slice('reply.'.length);
+    const responseMessage = fields.getTextInputValue('responseInput');
+    channel.send({
+      content: responseMessage,
+      reply: {messageReference: respondTo},
+    });
+  }
+};
 
 /**
  * @param {IContext} context
  * @return {boolean} - true if command is executed
  */
 export async function slashExecute(context) {
-  const {channel, lang, interaction} = context;
+  const {client, lang, interaction} = context;
+
+  const modalTitle = l10n.s(lang, `commands.${canonName}.modal-title`);
+  const responseLabel = l10n.s(lang, `commands.${canonName}.response-label`);
 
   if (context.userIsMod || context.hasAdminPermission) {
-    await interaction.deferReply({ephemeral: true});
-    const replyWith = interaction.options.get(optResponseLabel).value;
-    const replyTo = interaction.options.get(optMessageIdLabel).value;
-    const replyWhere = interaction.options.getChannel(optChannelLabel);
-    const responseError = l10n.s(lang, `commands.${canonName}.response-error`);
+    if (!eventHandlerAttached) {
+      client.on('interactionCreate', modalEventHandler);
+      eventHandlerAttached = true;
+    }
 
-    try {
-      if ((replyWhere) && (replyWhere.id !== channel.id)) {
-        const sentMessage = await replyWhere.send({
-          content: replyWith,
-          reply: {messageReference: replyTo, failIfNotExists: true},
-        });
-
-        interaction.editReply(
-            l10n.t(
-                lang,
-                `commands.${canonName}.response-sent-elsewhere`,
-                '{MESSAGE URL}',
-                sentMessage.url,
+    const modal = new Modal()
+        .setCustomId(`reply.${interaction.targetId}`)
+        .setTitle(modalTitle)
+        .addComponents(
+            new MessageActionRow().addComponents(
+                new TextInputComponent()
+                    .setCustomId('responseInput')
+                    .setLabel(responseLabel)
+                    .setStyle('PARAGRAPH')
+                    .setRequired(false),
             ),
         );
-      } else {
-        await channel.send({
-          content: replyWith,
-          reply: {messageReference: replyTo, failIfNotExists: true},
-        });
 
-        interaction.editReply(
-            l10n.s(lang, `commands.${canonName}.response-sent-here`),
-        );
-      }
-    } catch (error) {
-      interaction.editReply(responseError);
-    }
+    await interaction.showModal(modal);
     return true;
   } else {
     const noPermission = l10n.s(lang, 'messages.no-permission');
